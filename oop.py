78fa161d3df3d2e3383bc8ed75be6a7b7fafc447
Bulti <bulti.ta@gmail.com>
2020-11-07 07:10:19 +0100

    initial commit

---
+++ oop.py
@@ -0,0 +1,9 @@
+class Math:
+    note = 'this is the class variable'
+
+    # functions below are called methods within python class
+    def add(self, num1, num2):
+        print('addition: ', num1 + num2)
+
+    def multiply(self, num1, num2):
+        print('multiplication: ', num1 * num2)
